1410b69fb572ce84da334f534ad3feb2b080e3c1
Bamalla Keshk <basmallaabdelsalamkeshk@gmail.com>
2026-05-17 00:52:02 +0300

    Edit the config code

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 function getConfig() {
-  // We collected the values and ensured that they are integers, with a default value of 0 if the field is empty.
+  // We collected the values and ensured that they are integers, with a default value of 0
   return {
     rows: parseInt(document.getElementById('rows').value) || 0,
     cols: parseInt(document.getElementById('cols').value) || 0,
@@ -22,13 +22,12 @@ function validateConfig(config) {
     return false;
   }
 
-  // Checking the basic condition: (The total number of cards must be even)
   if (rows < 2 && cols < 2) {
     alert("The board is too small! Please make it at least 2x2 or 2x1.");
     return false;
   }
 
-  // Checking for sufficient time (optional but professional)
+  
   if (timeLimit < 10) {
     alert("Time limit is too short! Minimum recommended is 10 seconds.");
     return false;
